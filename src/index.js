ce4d3cac8aa0b562278ddd9846d81e1093a34df8
nastya-atro <belle.nastja@gmail.com>
2021-06-01 22:40:20 +0300

    create ws-chat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainApp from './chat-1-main/m1-ui/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ 
+    <MainApp/>, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+ 
